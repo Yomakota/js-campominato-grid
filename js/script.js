5c3e476d95a823fbe7a7f230016f14161353ebc0
Yomakota <teoperitore@outlook.com>
2022-06-14 15:17:08 +0200

    add comments

--- js/script.js
+++ js/script.js
@@ -10,3 +10,21 @@
 // Altrimenti il gioco va avanti a meno di aver raggiunto il numero massimo di tentativi possibili.In questo caso il gioco finisce con un messaggio di vittoria.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha scelto un numero che non era una bomba.
 
+//------------
+// Comments
+//------------
+// 1) Chiedo numero a utente per scegliere livello difficoltà con un prompt
+// 2) Range di numeri con cui giocare:
+    // se difficoltà 1 numeri range saranno tra 1 e 100;
+    // se difficoltà 2 numeri range saranno tra 1 e 81;
+    // se difficoltà 3 numeri range saranno tra 1 e 49;
+// 3) Creo 16 numeri casuali (bombe)
+// 4) Numero tentativi [numero max range - bombe(16)]
+
+//------------
+// Logica
+//------------
+//  Chiedere numero utente con prompt finchè:
+//  1) il numero inserito è una bomba (gioco finito, messaggio 'hai perso' e punteggio);
+//  Altrimenti:
+//  2) l'utente raggiunge numero massimo di tentativi (array con numeri tentativi utenti = numero max tentativi ) e gioco finito con messaggio 'Hai vinto'
